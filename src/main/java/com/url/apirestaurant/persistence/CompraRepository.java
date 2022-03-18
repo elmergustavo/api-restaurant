b772ba91b186e005bf140b32b1181b322479cc80
elmergustavo <elmergustavo79@gmail.com>
2022-03-18 16:24:27 -0600

    creacion de repositorios de compras

---
+++ src/main/java/com/url/apirestaurant/persistence/CompraRepository.java
@@ -0,0 +1,40 @@
+package com.url.apirestaurant.persistence;
+
+import com.url.apirestaurant.domain.Purchase;
+import com.url.apirestaurant.domain.repository.PurchaseRepository;
+import com.url.apirestaurant.persistence.crud.CompraCrudRepository;
+import com.url.apirestaurant.persistence.entity.Compra;
+import com.url.apirestaurant.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+    @Autowired
+    private CompraCrudRepository compraCrudRepository;
+
+    @Autowired
+    private PurchaseMapper mapper;
+
+    @Override
+    public List<Purchase> getAll() {
+        return mapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return compraCrudRepository.findByIdCliente(clientId)
+                .map(compras -> mapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra = mapper.toCompra(purchase);
+        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+
+        return mapper.toPurchase(compraCrudRepository.save(compra));
+    }
+}
---
+++ src/main/java/com/url/apirestaurant/persistence/crud/CompraCrudRepository.java
@@ -0,0 +1,11 @@
+package com.url.apirestaurant.persistence.crud;
+
+import com.url.apirestaurant.persistence.entity.Compra;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompraCrudRepository extends CrudRepository<Compra, Integer> {
+    Optional<List<Compra>> findByIdCliente(String idCliente);
+}
