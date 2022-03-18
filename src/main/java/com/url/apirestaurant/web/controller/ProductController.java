0811b3853af6017d1e9afae78ad8be6afbabee56
elmergustavo <elmergustavo79@gmail.com>
2022-03-18 00:22:37 -0600

    todo bien levantado

--- src/main/java/com/url/apirestaurant/web/controller/ProductController.java
+++ src/main/java/com/url/apirestaurant/web/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.url.apirestaurant.web.controller;
 import com.url.apirestaurant.domain.Product;
 import com.url.apirestaurant.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import java.util.Optional;
 public class ProductController {
     @Autowired
     private ProductService productService;
-
+    @GetMapping("/all")
     public List<Product> getAll() {
         return productService.getAll();
     }
